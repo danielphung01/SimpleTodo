fa02a6199fb62aa459de224c16f5337a722e551d
danielphung01 <danielphung01@gmail.com>
2021-02-01 00:47:38 -0800

    List title implemented

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
@@ -26,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String KEY_ITEM_TEXT = "item_text";
     public static final String KEY_ITEM_POSITION = "item_position";
+    public boolean isListName = false;
     public static final int EDIT_TEXT_CODE = 20;
 
-    List<String> items;
+    List<String> items;         // List without List Name for RecyclerView to display
+    List<String> saveList;      // List with List Name to be saved
 
+    TextView listName;
     Button btnAdd;
     EditText etItem;
     RecyclerView rvItems;
@@ -40,18 +44,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listName = findViewById(R.id.listName);
         btnAdd = findViewById(R.id.btnAdd);
         etItem = findViewById(R.id.etItem);
         rvItems = findViewById(R.id.rvItems);
 
-
         loadItems();
+        listName.setText(saveList.get(0));
+
+        // Edit List name
+        listName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = listName.getText().toString();
+                Log.d("MainActivity", "List name pressed");
+
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                // pass the data being edited
+                isListName = true;
+                i.putExtra(KEY_ITEM_TEXT, name);
+                i.putExtra(KEY_ITEM_POSITION, 0);
+                // display the activity
+                startActivityForResult(i, EDIT_TEXT_CODE);
+            }
+        });
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
             @Override
             public void onItemLongClicked(int position) {
                 // Delete the item from the model
                 items.remove(position);
+                saveList.remove(position + 1);
                 // Notify the adapter
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
@@ -66,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
                 // create the new activity
                 Intent i = new Intent(MainActivity.this, EditActivity.class);
                 // pass the data being edited
+                isListName = false;
                 i.putExtra(KEY_ITEM_TEXT, items.get(position));
                 i.putExtra(KEY_ITEM_POSITION, position);
                 // display the activity
@@ -82,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 String todoItem = etItem.getText().toString();
                 // Add it to the model
                 items.add(todoItem);
+                saveList.add(todoItem);
                 // Notify adapter that an item is inserted
                 itemsAdapter.notifyItemInserted(items.size() - 1);
                 etItem.setText("");
@@ -102,24 +127,33 @@ public class MainActivity extends AppCompatActivity {
             // Retrieve the updated text value
             String itemText = data.getStringExtra(KEY_ITEM_TEXT);
             // extract the original position of the edited item from the position key
-            int position = data.getExtras().getInt(KEY_ITEM_POSITION);
 
-            if (itemText.length() == 0) {
-                // Remove item if item is changed to nothing
-                items.remove(position);
-                itemsAdapter.notifyItemRemoved(position);
-                Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+            if (isListName) {
+                listName.setText(itemText);
+                saveList.set(0, itemText);
                 saveItems();
+                Toast.makeText(getApplicationContext(), "List name updated successfully", Toast.LENGTH_SHORT).show();
             } else {
-                // update the model at the right position with new item text
-                items.set(position, itemText);
-                // notify the adapter
-                itemsAdapter.notifyItemChanged(position);
-                // persist the changes
-                saveItems();
-                Toast.makeText(getApplicationContext(), "Item updated successfully", Toast.LENGTH_SHORT).show();
-            }
+                int position = data.getExtras().getInt(KEY_ITEM_POSITION);
 
+                if (itemText.length() == 0) {
+                    // Remove item if item is changed to nothing
+                    items.remove(position);
+                    saveList.remove(position + 1);
+                    itemsAdapter.notifyItemRemoved(position);
+                    Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+                    saveItems();
+                } else {
+                    // update the model at the right position with new item text
+                    items.set(position, itemText);
+                    saveList.set(position + 1, itemText);
+                    // notify the adapter
+                    itemsAdapter.notifyItemChanged(position);
+                    // persist the changes
+                    saveItems();
+                    Toast.makeText(getApplicationContext(), "Item updated successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
         } else {
             Log.w("MainActivity", "Unknown call to onActivityResult");
         }
@@ -132,17 +166,21 @@ public class MainActivity extends AppCompatActivity {
     // This function will load items by reading every line of the data file
     private void loadItems() {
         try {
+            saveList = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
             items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+            items.remove(0);
         } catch (IOException e) {
             Log.e("MainActivity", "Error reading items", e);
             items = new ArrayList<>();
+            saveList = new ArrayList<>();
+            saveList.add("");
         }
     }
 
     // This function saves items by writing them into the data file
     private void saveItems() {
         try {
-            FileUtils.writeLines(getDataFile(), items);
+            FileUtils.writeLines(getDataFile(), saveList);
         } catch (IOException e) {
             Log.e("MainActivity", "Error writing items", e);
         }
